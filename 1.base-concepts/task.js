3a71d227d637f2f50a2bb3eba9b04f008b957833
Fundaypro <fundaypro123@gmail.com>
2021-09-09 17:22:19 +0200

    правки по первой задаче

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -13,15 +13,10 @@ function solveEquation(a, b, c) {
     arr.push((b * -1) / (2*a));
     return arr;
 
-  } else if (D < 0) {
-    return arr;
-  }
-  
-
-  return arr;
+  } else return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
+//function calculateTotalMortgage(percent, contribution, amount, date) {
   // if(typeof moth)
   // let moth = (date.getFullYear() - new Date().getFullYear()) *12
   // console.log(moth)
@@ -31,5 +26,5 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   // let pay = S * (P + P / (((1 + P)^n) - 1));
   //     pay = Math.floor(pay*100)/100
   // return pay*moth
-}
+//}
 //calculateTotalMortgage(10,1000,50000,12)
